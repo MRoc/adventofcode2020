ad46776979f7564dc97945006ce6a95c24a621ae
Matthias Mayrock <mma@templafy.com>
2020-12-06 10:42:10 +0100

    Simplified

--- Puzzle03/Program.cs
+++ Puzzle03/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 
 namespace Puzzle03
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] _)
+        // 1/1: Found 68 trees(1)
+        // 3/1: Found 203 trees(1)
+        // 5/1: Found 78 trees(1)
+        // 7/1: Found 77 trees(1)
+        // 1/2: Found 40 trees(1)
+        // Result: 3316272960
+        static void Main()
         {
-            var grid = SplitMaze(LoadInput());
-
-            var result = MultiplyNumberOfTreesOnDifferentSlopes(grid);
-
+            var result = MultiplyNumberOfTreesOnDifferentSlopes(SplitInput(LoadInput()));
             Console.WriteLine($"Result: {result}");
         }
 
@@ -24,55 +28,44 @@ namespace Puzzle03
             return reader.ReadToEnd();
         }
 
-        private static char[][] SplitMaze(string input)
+        private static string[] SplitInput(string input)
         {
             return input
                 .Split("\n")
                 .Where(l => !string.IsNullOrWhiteSpace(l))
-                .Select(l => l.ToArray())
                 .ToArray();
         }
 
-        private static long MultiplyNumberOfTreesOnDifferentSlopes(char[][] grid)
+        private static long MultiplyNumberOfTreesOnDifferentSlopes(string[] grid)
         {
-            long result = 1;
+            return new[]
+                {
+                    Tuple.Create(1, 1),
+                    Tuple.Create(3, 1),
+                    Tuple.Create(5, 1),
+                    Tuple.Create(7, 1),
+                    Tuple.Create(1, 2),
+                }
+                .Select(s => Count(grid, s.Item1, s.Item2))
+                .Aggregate(1l, (a, b) => a * b);
+        }
 
-            foreach (var s in _slopes)
-            {
-                var trees = CountTrees(grid, s.Item1, s.Item2);
-                Console.WriteLine($"{s.Item1}/{s.Item2}: Found {trees} trees (1)");
-
-                result *= trees;
-            }
-
+        private static int Count(string[] grid, int dX, int dY)
+        {
+            var result = Walk(grid, dX, dY).Count(c => c == '#');
+            Console.WriteLine($"{dX}/{dY}: Found {result} trees (1)");
             return result;
         }
 
-        private static int CountTrees(char[][] grid, int dX, int dY)
+        private static IEnumerable<char> Walk(string[] grid, int dX, int dY)
         {
             var width = grid[0].Length;
             var height = grid.Length;
 
-            var count = 0;
-
-            for (int x = 0, y = 0;  y < height; x = (x + dX) % width, y += dY)
+            for (int x = 0, y = 0; y < height; x = (x + dX) % width, y += dY)
             {
-                if (grid[y][x] == '#')
-                {
-                    count++;
-                }
+                yield return grid[y][x];
             }
-
-            return count;
         }
-
-        private static readonly Tuple<int, int>[] _slopes = new Tuple<int, int>[]
-        {
-            Tuple.Create(1, 1),
-            Tuple.Create(3, 1),
-            Tuple.Create(5, 1),
-            Tuple.Create(7, 1),
-            Tuple.Create(1, 2),
-        };
     }
 }
