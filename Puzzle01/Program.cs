d1a107ee12d42e6a693c796bf5863398012e2eb4
Matthias Mayrock <mma@templafy.com>
2020-12-06 12:20:18 +0100

    Such a nice little LINQ

--- Puzzle01/Program.cs
+++ Puzzle01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -8,36 +9,34 @@ namespace Puzzle01
     // https://adventofcode.com/2020/day/1
     static class Program
     {
-        static void Main(string[] _)
+        // 245+131+1644=2020 -> 245*131*1644=52764180
+        // 245+1644+131=2020 -> 245*1644*131=52764180
+        // 131+245+1644=2020 -> 131*245*1644=52764180
+        // 131+1644+245=2020 -> 131*1644*245=52764180
+        // 1644+245+131=2020 -> 1644*245*131=52764180
+        // 1644+131+245=2020 -> 1644*131*245=52764180
+        static void Main()
         {
-            var numbers = LoadInput()
+            foreach (var t in LoadInput()
                 .Split('\n')
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 .Select(l => int.Parse(l))
                 .Distinct()
-                .ToArray();
-
-            foreach (var n0 in numbers)
+                .Permutate()
+                .Where(t => t.Item1 + t.Item2 + t.Item3 == 2020))
             {
-                foreach (var n1 in numbers)
-                {
-                    foreach (var n2 in numbers)
-                    {
-                        if (n0 + n1 + n2 == 2020)
-                        {
-                            Console.WriteLine($"{n0}+{n1}+{n2}=2020 -> {n0}*{n1}*{n2}={n0 * n1 * n2}");
-                        }
-                    }
-                }
+                Console.WriteLine($"{t.Item1}+{t.Item2}+{t.Item3}=2020 -> {t.Item1}*{t.Item2}*{t.Item3}={t.Item1 * t.Item2 * t.Item3}");
             }
+        }
 
-            Console.ReadKey();
+        private static IEnumerable<Tuple<T, T, T>> Permutate<T>(this IEnumerable<T> seq)
+        {
+            return seq.SelectMany(e0 => seq.SelectMany(e1 => seq.Select(e2 => Tuple.Create(e0, e1, e2))));
         }
 
         private static string LoadInput()
         {
-            var assembly = Assembly.GetCallingAssembly();
-            using var stream = assembly.GetManifestResourceStream("Puzzle01.input.txt");
+            using var stream = Assembly.GetCallingAssembly().GetManifestResourceStream("Puzzle01.input.txt");
             using var reader = new StreamReader(stream);
             return reader.ReadToEnd();
         }
