88bf933639ba698bdf85cbc732a86b0f294027b1
Matthias Mayrock <mma@templafy.com>
2020-12-14 12:50:19 +0100

    Simplified

--- Puzzles/Day14.cs
+++ Puzzles/Day14.cs
@@ -11,27 +11,28 @@ namespace Puzzles
     {
         public static long Puzzle1()
         {
-            var memory = new Dictionary<int, long>();
+            var memory = new Dictionary<long, long>();
 
-            var maskOr = 0x0000000000000000UL;
-            var maskAnd = 0xFFFFFFFFFFFFFFFFUL;
+            var maskOr = default(long);
+            var maskAnd = default(long);
 
             foreach (var line in Input.LoadLines("Puzzles.Input.input14.txt"))
             {
                 var match = MaskDecoder.Match(line);
                 if (match.Success)
                 {
-                    maskOr = Convert.ToUInt64(match.Groups[1].Value.Replace('X', '0'), 2);
-                    maskAnd = ~Convert.ToUInt64(match.Groups[1].Value.Replace('1', 'X').Replace('0', '1').Replace('X', '0'), 2);
+                    var mask = match.Groups[1].Value;
+                    maskOr = Convert.ToInt64(mask.Replace('X', '0'), 2);
+                    maskAnd = ~Convert.ToInt64(mask.Replace('1', 'X').Replace('0', '1').Replace('X', '0'), 2);
                 }
                 else
                 {
                     var result = AssignmentDecoder.Match(line);
-                    var address = int.Parse(result.Groups[1].Value);
+                    var address = long.Parse(result.Groups[1].Value);
                     var value = long.Parse(result.Groups[2].Value);
-                    memory[address] = value & (long) maskAnd | (long) maskOr;
+                    memory[address] = value & maskAnd | maskOr;
                 }
-            } 
+            }
 
             return memory.Values.Sum();
         }
@@ -52,12 +53,19 @@ namespace Puzzles
                 else
                 {
                     var result = AssignmentDecoder.Match(line);
+                    var address = long.Parse(result.Groups[1].Value);
                     var value = long.Parse(result.Groups[2].Value);
-                    var addresses = PermutateAddress(long.Parse(result.Groups[1].Value), mask);
-
-                    foreach (var address in addresses)
+                    
+                    foreach (var singleAddress in PermuteAddress(
+                        address | Convert.ToInt64(mask.Replace('X', '0'), 2),
+                        mask
+                            .Reverse()
+                            .Select((c, i) => (c, i))
+                            .Where(t => t.c == 'X')
+                            .Select(t => 0x1L << t.i)
+                            .ToArray()))
                     {
-                        memory[address] = value;
+                        memory[singleAddress] = value;
                     }
                 }
             }
@@ -65,35 +73,17 @@ namespace Puzzles
             return memory.Values.Sum();
         }
 
-        private static IEnumerable<long> PermutateAddress(long address, string bitmask)
+        private static IEnumerable<long> PermuteAddress(long address, IEnumerable<long> floatingBits)
         {
-            var maskOr = Convert.ToInt64(bitmask.Replace('X', '0'), 2);
-
-            address |= maskOr;
-
-            var floatingBits = bitmask
-                .Reverse()
-                .Select((c, i) => (c: c, i: i, bitmask: bitmask))
-                .Where(t => t.c == 'X')
-                .ToArray();
-
-            var addresses = PermutateAddress(address, 0, floatingBits).ToArray();
-            return addresses;
-        }
-
-        private static IEnumerable<long> PermutateAddress(long address, int index, IReadOnlyList<(char c, int i, string bitmask)> items)
-        {
-            if (index < items.Count)
+            if (floatingBits.Any())
             {
-                var item = items[index];
+                var bit = floatingBits.First();
 
-                var bit = 0x1L << item.i;
-
-                foreach (var r in PermutateAddress(address & ~bit, index + 1, items))
+                foreach (var r in PermuteAddress(address & ~bit, floatingBits.Skip(1)))
                 {
                     yield return r;
                 }
-                foreach (var r in PermutateAddress(address | bit, index + 1, items))
+                foreach (var r in PermuteAddress(address | bit, floatingBits.Skip(1)))
                 {
                     yield return r;
                 }
@@ -103,8 +93,6 @@ namespace Puzzles
                 yield return address;
             }
         }
-
-
 
         private static readonly Regex MaskDecoder = new Regex(@"^mask\s=\s([0|1|X]+)\b");
 
