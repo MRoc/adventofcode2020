5957b5de56af2af7ab398b743bc78986da8e251a
Matthias Mayrock <mma@templafy.com>
2020-12-14 10:14:42 +0100

    What a hack

--- Puzzles/Day14.cs
+++ Puzzles/Day14.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace Puzzles
 {
     // Puzzle 1: 10885823581193
-    // Puzzle 2: 0
+    // Puzzle 2: 3816594901962
     public static class Day14
     {
         public static long Puzzle1()
@@ -36,13 +36,78 @@ namespace Puzzles
             return memory.Values.Sum();
         }
 
+        public static long Puzzle2()
+        {
+            var memory = new Dictionary<long, long>();
+
+            var mask = string.Empty;
+
+            foreach (var line in Input.LoadLines("Puzzles.Input.input14.txt"))
+            {
+                var match = MaskDecoder.Match(line);
+                if (match.Success)
+                {
+                    mask = match.Groups[1].Value;
+                }
+                else
+                {
+                    var result = AssignmentDecoder.Match(line);
+                    var value = long.Parse(result.Groups[2].Value);
+                    var addresses = PermutateAddress(long.Parse(result.Groups[1].Value), mask);
+
+                    foreach (var address in addresses)
+                    {
+                        memory[address] = value;
+                    }
+                }
+            }
+
+            return memory.Values.Sum();
+        }
+
+        private static IEnumerable<long> PermutateAddress(long address, string bitmask)
+        {
+            var maskOr = Convert.ToInt64(bitmask.Replace('X', '0'), 2);
+
+            address |= maskOr;
+
+            var floatingBits = bitmask
+                .Reverse()
+                .Select((c, i) => (c: c, i: i, bitmask: bitmask))
+                .Where(t => t.c == 'X')
+                .ToArray();
+
+            var addresses = PermutateAddress(address, 0, floatingBits).ToArray();
+            return addresses;
+        }
+
+        private static IEnumerable<long> PermutateAddress(long address, int index, IReadOnlyList<(char c, int i, string bitmask)> items)
+        {
+            if (index < items.Count)
+            {
+                var item = items[index];
+
+                var bit = 0x1L << item.i;
+
+                foreach (var r in PermutateAddress(address & ~bit, index + 1, items))
+                {
+                    yield return r;
+                }
+                foreach (var r in PermutateAddress(address | bit, index + 1, items))
+                {
+                    yield return r;
+                }
+            }
+            else
+            {
+                yield return address;
+            }
+        }
+
+
+
         private static readonly Regex MaskDecoder = new Regex(@"^mask\s=\s([0|1|X]+)\b");
 
         private static readonly Regex AssignmentDecoder = new Regex(@"^mem\[([0-9]+)\]\s=\s([0-9]+)\b");
-
-        public static long Puzzle2()
-        {
-            return 0;
-        }
     }
 }
