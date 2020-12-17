34381adfae570a46d1f72e7f79bc814a61af5bb2
Matthias Mayrock <mma@templafy.com>
2020-12-17 09:16:12 +0100

    Brute force 4D

--- Puzzles/Day17.cs
+++ Puzzles/Day17.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace Puzzles
 {
     // Puzzle 1: 240
-    // Puzzle 2: 0
+    // Puzzle 2: 1180
     public static class Day17
     {
         public static long Puzzle1()
@@ -14,7 +14,7 @@ namespace Puzzles
 
             foreach (var i in Enumerable.Range(0, 6))
             {
-                state = state.NextState();
+                state = state.NextState3();
             }
 
             return state.Length;
@@ -22,7 +22,13 @@ namespace Puzzles
 
         public static long Puzzle2()
         {
-            return 0;
+            var state = LoadInput().Select(p => new Point4(p.X, p.Y, 0, 0)).ToArray();
+            foreach (var i in Enumerable.Range(0, 6))
+            {
+                state = state.NextState4();
+            }
+
+            return state.Length;
         }
 
         private static Point3[] LoadInput()
@@ -36,7 +42,7 @@ namespace Puzzles
                 .ToArray();
         }
 
-        private static Point3[] NextState(this Point3[] input)
+        private static Point3[] NextState3(this Point3[] input)
         {
             var output = new HashSet<Point3>();
 
@@ -49,6 +55,30 @@ namespace Puzzles
                 }
             }
             
+            foreach (var adjacentCell in input
+                .SelectMany(p => p.AdjacentCells())
+                .Distinct()
+                .Where(c0 => c0.AdjacentCells().Count(input.Contains) == 3))
+            {
+                output.Add(adjacentCell);
+            }
+
+            return output.ToArray();
+        }
+
+        private static Point4[] NextState4(this Point4[] input)
+        {
+            var output = new HashSet<Point4>();
+
+            foreach (var activeCells in input)
+            {
+                var activeAdjacentCells = activeCells.AdjacentCells().Count(input.Contains);
+                if (2 <= activeAdjacentCells && activeAdjacentCells <= 3)
+                {
+                    output.Add(activeCells);
+                }
+            }
+
             foreach (var adjacentCell in input
                 .SelectMany(p => p.AdjacentCells())
                 .Distinct()
@@ -73,6 +103,29 @@ namespace Puzzles
                             if (x != X || y != Y || z != Z)
                             {
                                 yield return new Point3(x, y, z);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public record Point4(int X, int Y, int Z, int W)
+        {
+            public IEnumerable<Point4> AdjacentCells()
+            {
+                for (int x = X - 1; x <= X + 1; ++x)
+                {
+                    for (int y = Y - 1; y <= Y + 1; ++y)
+                    {
+                        for (int z = Z - 1; z <= Z + 1; ++z)
+                        {
+                            for (int w = W - 1; w <= W + 1; ++w)
+                            {
+                                if (x != X || y != Y || z != Z || w != W)
+                                {
+                                    yield return new Point4(x, y, z, w);
+                                }
                             }
                         }
                     }
