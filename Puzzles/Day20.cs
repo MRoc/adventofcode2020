2de966429b69a5f46df2a129de7dba86952342dc
Matthias Mayrock <mma@templafy.com>
2020-12-20 09:17:04 +0100

    Day 20 stub

---
+++ Puzzles/Day20.cs
@@ -0,0 +1,18 @@
+﻿namespace Puzzles
+{
+    // Puzzle 1: 0
+    // Puzzle 2: 0
+    public static class Day20
+    {
+        public static long Puzzle1()
+        {
+            return 0;
+        }
+
+        public static long Puzzle2()
+        {
+
+            return 0;
+        }
+    }
+}
