dc01a0bb450ad44b2d41036c5adbfded1e45a1a2
Matthias Mayrock <mma@templafy.com>
2020-12-24 10:07:10 +0100

    Part 2

--- Puzzles/Day24.cs
+++ Puzzles/Day24.cs
@@ -20,7 +20,21 @@ namespace Puzzles
 
         public static long Puzzle2()
         {
-            return 0;
+            var state = Input
+                .LoadLines("Puzzles.Input.input24.txt")
+                .ParsePaths()
+                .Select(path => path.Aggregate((x: 0, y: 0), Step))
+                .GroupBy(p => p)
+                .Where(g => g.Count() % 2 == 1)
+                .Select(g => g.Key)
+                .ToArray();
+            
+            foreach (var _ in Enumerable.Range(0, 100))
+            {
+                state = state.NextState().Distinct().ToArray();
+            }    
+            
+            return state.Length;
         }
 
         private static string[][] ParsePaths(this IEnumerable<string> lines)
@@ -52,6 +66,31 @@ namespace Puzzles
             "e"  => (x: p.x + 1,  y: p.y + 0),
             _    => throw new NotSupportedException()
         };
+
+        private static IEnumerable<(int x, int y)> AdjacentTiles(this (int x, int y) tile)
+        {
+            return Directions.Select(d => Step(tile, d));
+        }
         
+        private static IEnumerable<(int x, int y)> NextState(this (int x, int y)[] state)
+        {
+            foreach (var tile in state)
+            {
+                var countAdjacentTiles = tile.AdjacentTiles().Count(p => state.Contains(p));
+                if (countAdjacentTiles > 0 && countAdjacentTiles <= 2)
+                {
+                    yield return tile;
+                }
+                
+                foreach (var adjacentTile in tile.AdjacentTiles().Where(t => !state.Contains(t))) 
+                {
+                    var c = adjacentTile.AdjacentTiles().Count(p => state.Contains(p));
+                    if (c == 2)
+                    {
+                        yield return adjacentTile;
+                    }
+                }
+            }
+        }
     }
 }
